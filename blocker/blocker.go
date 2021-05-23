ab92beaed4fea447b74aa84717b57ee801c17c62
Viktor Mörsell <viktor.morsell@northvolt.com>
2021-05-23 02:48:47 +0200

    Add interface

---
+++ blocker/blocker.go
@@ -0,0 +1,20 @@
+package blocker
+
+import "sync"
+
+type Blocker interface {
+	AddIP(ip string) error
+}
+
+type blocker struct {
+	ips   map[string]struct{}
+	hosts map[string]struct{}
+	mu    sync.RWMutex
+}
+
+func New() Blocker {
+	return &blocker{
+		ips:   make(map[string]struct{}),
+		hosts: make(map[string]struct{}),
+	}
+}
